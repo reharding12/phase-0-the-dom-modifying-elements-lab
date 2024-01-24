b6f8c77b3d48bf16df88fc097ff72a7db245e2c1
Roxanne <reharding12@gmail.com>
2024-01-23 19:26:00 -0700

    passed 4 tests

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-// Write your code here!
+const mainElement = document.querySelector('main#main');
+mainElement.remove();
+
+const newHeader = document.querySelector('h1');
+newHeader.id = 'victory';
+   
+
+   const newHeaderText = newHeader.innerHTML;
+   console.log(newHeaderText.includes('is the champion'));
